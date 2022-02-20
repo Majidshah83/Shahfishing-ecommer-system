96704a8819e90d3d06687fabb405219519b48a57
Majidshah83 <shahmajid508@gmail.com>
2022-02-20 15:40:54 +0500

    Template Integration Laravel

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,9 @@
+        <script src="{{asset('layouts')}}/vendor/jquery/jquery.min.js"></script>
+        <script src="{{asset('layouts')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="{{asset('layouts')}}/vendor/lightbox2/js/lightbox.min.js"></script>
+        <script src="{{asset('layouts')}}/vendor/nouislider/nouislider.min.js"></script>
+        <script src="{{asset('layours')}}/vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
+        <script src="{{asset('layouts')}}/vendor/owl.carousel2/owl.carousel.min.js"></script>
+        <script src="{{asset('layouts')}}/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"></script>
+        <script src="js/front.js"></script>
+        <script>
---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,10 @@
+    <link rel="stylesheet" href="{{asset('layouts')}}/vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Lightbox-->
+    <link rel="stylesheet" href="{{asset('layouts')}}/vendor/lightbox2/css/lightbox.min.css">
+    <!-- Range slider-->
+    <link rel="stylesheet" href="{{asset('layouts')}}/vendor/nouislider/nouislider.min.css">
+    <!-- Bootstrap select-->
+    <link rel="stylesheet" href="{{asset('layouts')}}/vendor/bootstrap-select/css/bootstrap-select.min.css">
+    <!-- Owl Carousel-->
+    <link rel="stylesheet" href="{{asset('layouts')}}/vendor/owl.carousel2/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset('layouts')}}/vendor/owl.carousel2/assets/owl.theme.default.css">
